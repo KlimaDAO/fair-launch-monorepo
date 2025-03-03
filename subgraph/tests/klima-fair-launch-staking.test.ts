f95596f9276ec0805fc87af6faa869e3ce527c17
psparacino <tufnelenterprises@gmail.com>
2025-03-03 15:55:01 -0500

    helpers file

--- subgraph/tests/klima-fair-launch-staking.test.ts
+++ subgraph/tests/klima-fair-launch-staking.test.ts
@@ -17,119 +17,13 @@ import {
   handleStakeClaimed,
 } from "../src/klima-fair-launch-staking";
 import { Stake, Wallet } from "../generated/schema";
+import {
+  createStakeClaimedEvent,
+  createStakeCreatedEvent,
+} from "./utils/helpers";
+import { createStake, createStakeBurnedEvent } from "./utils/helpers";
 
 const user = Address.fromString("0xca4793c93a94e7a70a4631b1cece6546e76eb19e");
-
-// event helpers
-
-function createStakeCreatedEvent(
-  user: Address,
-  amount: BigInt,
-  multiplier: BigInt,
-  startTimestamp: BigInt,
-  txHash: Bytes
-): StakeCreatedEvent {
-  let event = changetype<StakeCreatedEvent>(newMockEvent());
-  event.parameters = new Array();
-  event.parameters.push(
-    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
-  );
-  event.parameters.push(
-    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
-  );
-  event.parameters.push(
-    new ethereum.EventParam(
-      "multiplier",
-      ethereum.Value.fromUnsignedBigInt(multiplier)
-    )
-  );
-  event.parameters.push(
-    new ethereum.EventParam(
-      "startTimestamp",
-      ethereum.Value.fromUnsignedBigInt(startTimestamp)
-    )
-  );
-  event.transaction.hash = txHash;
-  return event;
-}
-
-function createStakeBurnedEvent(
-  user: Address,
-  burnAmount: BigInt,
-  txHash: Bytes
-): StakeBurnedEvent {
-  let event = changetype<StakeBurnedEvent>(newMockEvent());
-  event.parameters = new Array();
-  event.parameters.push(
-    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
-  );
-  event.parameters.push(
-    new ethereum.EventParam(
-      "burnAmount",
-      ethereum.Value.fromUnsignedBigInt(burnAmount)
-    )
-  );
-  event.transaction.hash = txHash;
-  return event;
-}
-
-function createStakeClaimedEvent(
-  user: Address,
-  klimaAllocation: BigInt,
-  klimaXAllocation: BigInt,
-  txHash: Bytes
-): StakeClaimedEvent {
-  let event = changetype<StakeClaimedEvent>(newMockEvent());
-  event.parameters = new Array();
-  event.parameters.push(
-    new ethereum.EventParam("user", ethereum.Value.fromAddress(user))
-  );
-
-  event.parameters.push(
-    new ethereum.EventParam(
-      "totalUserStaked",
-      ethereum.Value.fromUnsignedBigInt(BigInt.zero())
-    )
-  );
-  event.parameters.push(
-    new ethereum.EventParam(
-      "klimaAllocation",
-      ethereum.Value.fromUnsignedBigInt(klimaAllocation)
-    )
-  );
-  event.parameters.push(
-    new ethereum.EventParam(
-      "klimaXAllocation",
-      ethereum.Value.fromUnsignedBigInt(klimaXAllocation)
-    )
-  );
-
-  event.parameters.push(
-    new ethereum.EventParam(
-      "timestamp",
-      ethereum.Value.fromUnsignedBigInt(BigInt.zero())
-    )
-  );
-  event.transaction.hash = txHash;
-  return event;
-}
-
-function createStake(
-  wallet: Wallet,
-  amount: BigInt,
-  multiplier: BigInt,
-  startTimestamp: BigInt,
-  txHash: Bytes
-): Stake {
-  let stake = new Stake(txHash);
-  stake.wallet = wallet.id;
-  stake.amount = amount;
-  stake.multiplier = multiplier;
-  stake.startTimestamp = startTimestamp;
-  stake.stakeCreationHash = txHash;
-  stake.save();
-  return stake;
-}
 
 test("handleStakeCreated creates a wallet and stake", () => {
   let amount = BigInt.fromI32(100);
