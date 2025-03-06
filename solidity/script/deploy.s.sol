bf8e844bc64b8fc5a70a71397e86bdc458b536f4
psparacino <tufnelenterprises@gmail.com>
2025-03-06 14:52:48 -0500

    working makefile

--- solidity/script/deploy.s.sol
+++ solidity/script/deploy.s.sol
@@ -11,17 +11,11 @@ import {console2} from "forge-std/console2.sol";
 
 contract Deploy is Script {
 
-    address USER_2 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
-    address USER_3 = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359;
-    address USER_4 = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
-    address USER_5 = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
-
     function run() public {
         vm.startBroadcast();
 
-        // only necessary if we use sepolia
-        // BaseERC20 oldKlima = new BaseERC20("oldKlima", "KLIMA_V0", 1000000 * 10 ** 18);
-        // console2.log("old Klima deployed at:", address(oldKlima));
+        BaseERC20 oldKlima = new BaseERC20("oldKlima", "KLIMA_V0", 1000000 * 10 ** 18);
+        console2.log("old Klima deployed at:", address(oldKlima));
 
         BaseERC20 newKlima = new BaseERC20("Klima", "KLIMA", 1000000 * 10 ** 18);
         console2.log("new Klima deployed at:", address(newKlima));
