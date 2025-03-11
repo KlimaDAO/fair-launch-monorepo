18e24eb93b6a79b38bbebbed5f1d773f70c96d76
0xMakka <keith.33m@gmail.com>
2025-03-11 13:05:14 +0000

    chore: cleanup wagmi config, fix favicon

--- fair-launch/utils/wagmi.ts
+++ fair-launch/utils/wagmi.ts
@@ -1,20 +1,15 @@
-import { base, baseSepolia } from 'wagmi/chains';
 import { Config } from 'wagmi';
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { 
-  createConfig, 
-  http, 
+import { baseSepolia } from 'wagmi/chains';
+import {
+  createConfig,
+  http,
   cookieStorage,
-  createStorage 
+  createStorage
 } from 'wagmi'
 
 export const config: Config = createConfig({
   ssr: true,
   chains: [baseSepolia],
-  // appName: 'Klima Fair Launch dApp',
-  // projectId: 'YOUR_PROJECT_ID', // todo - replace with your project id
   storage: createStorage({ storage: cookieStorage }),
-  transports: {
-    [baseSepolia.id]: http(),
-  },
+  transports: { [baseSepolia.id]: http() },
 });
