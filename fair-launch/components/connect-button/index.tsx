3ff552f1ccf63c3405131cd88f4e4ace8fa1aec8
0xMakka <keith.33m@gmail.com>
2025-03-14 12:41:07 +0000

    fix: redirect to my-rewards on connect

--- fair-launch/components/connect-button/index.tsx
+++ fair-launch/components/connect-button/index.tsx
@@ -1,63 +1,74 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { ConnectButton as ConnectButtonRainbow } from "@rainbow-me/rainbowkit";
 import * as styles from "./styles";
 
-export const ConnectButton = () => (
-  <ConnectButtonRainbow.Custom>
-    {({ account, chain, openChainModal, openConnectModal, mounted }) => {
-      const ready = mounted;
-      const connected = ready && account && chain;
-      return (
-        <div
-          {...(!ready && {
-            "aria-hidden": true,
-            style: {
-              opacity: 0,
-              pointerEvents: "none",
-              userSelect: "none",
-            },
-          })}
-        >
-          {(() => {
-            if (!connected) {
+export const ConnectButton = () => {
+  const router = useRouter();
+  return (
+    <ConnectButtonRainbow.Custom>
+      {({ account, chain, openChainModal, openConnectModal, mounted }) => {
+        const ready = mounted;
+        const connected = ready && account && chain;
+        // after successful connection, redirect to /my-rewards
+        if (connected) {
+          setTimeout(() => {
+            router.push('/my-rewards')
+          }, 1);
+        }
+
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <button
+                    onClick={openConnectModal}
+                    type="button"
+                    className={styles.connectButton}
+                  >
+                    Connect
+                  </button>
+                );
+              }
+              if (chain.unsupported) {
+                return (
+                  <button
+                    onClick={openChainModal}
+                    type="button"
+                    className={styles.wrongNetworkButton}
+                  >
+                    Wrong network
+                  </button>
+                );
+              }
               return (
-                <button
-                  onClick={openConnectModal}
-                  type="button"
-                  className={styles.connectButton}
-                >
-                  Connect
-                </button>
+                <div style={{ display: "flex", gap: 12 }}>
+                  <button
+                    onClick={openChainModal}
+                    style={{ display: "flex", alignItems: "center" }}
+                    type="button"
+                    className={styles.connectButton}
+                  >
+                    {account.displayName}
+                    {account.displayBalance ? ` (${account.displayBalance})` : ""}
+                  </button>
+                </div>
               );
-            }
-            if (chain.unsupported) {
-              return (
-                <button
-                  onClick={openChainModal}
-                  type="button"
-                  className={styles.wrongNetworkButton}
-                >
-                  Wrong network
-                </button>
-              );
-            }
-            return (
-              <div style={{ display: "flex", gap: 12 }}>
-                <button
-                  onClick={openChainModal}
-                  style={{ display: "flex", alignItems: "center" }}
-                  type="button"
-                  className={styles.connectButton}
-                >
-                  {account.displayName}
-                  {account.displayBalance ? ` (${account.displayBalance})` : ""}
-                </button>
-              </div>
-            );
-          })()}
-        </div>
-      );
-    }}
-  </ConnectButtonRainbow.Custom>
-);
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButtonRainbow.Custom>
+  )
+};
