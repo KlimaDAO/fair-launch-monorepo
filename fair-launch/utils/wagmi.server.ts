c9b1af336974a0e35b380bab85c639fe2f91471f
0xMakka <keith.33m@gmail.com>
2025-03-20 22:14:25 +0000

    fix: wagmi config

--- fair-launch/utils/wagmi.server.ts
+++ fair-launch/utils/wagmi.server.ts
@@ -3,13 +3,10 @@ import { baseSepolia } from 'wagmi/chains';
 import {
   createConfig,
   http,
-  cookieStorage,
-  createStorage,
 } from 'wagmi'
 
 export const config: Config = createConfig({
   ssr: true,
   chains: [baseSepolia],
-  storage: createStorage({ storage: cookieStorage }),
   transports: { [baseSepolia.id]: http() },
 });
