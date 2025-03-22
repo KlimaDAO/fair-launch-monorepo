c0b3ecec41d2172082e4ca1a049261bc6907d025
0xMakka <keith.33m@gmail.com>
2025-03-22 21:21:00 +0000

    chore: temp add logout on mobile

--- fair-launch/components/navbar/index.tsx
+++ fair-launch/components/navbar/index.tsx
@@ -5,12 +5,33 @@ import Image from "next/image";
 import klimaLogo from "@public/kp-logo.svg";
 import { LuMenu } from "react-icons/lu";
 import * as styles from "./styles";
+import { MdLogout } from "react-icons/md";
+import { useAccount, useDisconnect } from "wagmi";
+import { useRouter } from "next/navigation";
 
-export const Navbar: FC = () => (
-  <div className={styles.navbar}>
-    <Image src={klimaLogo} alt="Klima Protocol Logo" />
-    <div className={styles.menuContainer}>
-      <LuMenu className={styles.menuIcon} />
+export const Navbar: FC = () => {
+  const router = useRouter();
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
+
+  const handleLogout = async () => {
+    await disconnect();
+    setTimeout(() => router.push('/'), 100);
+  };
+
+  return (
+    <div className={styles.navbar}>
+      <Image src={klimaLogo} alt="Klima Protocol Logo" />
+      <div className={styles.menuContainer}>
+        {/* <LuMenu className={styles.menuIcon} /> */}
+        {address && (
+          <button
+            onClick={handleLogout}>
+            <LuMenu className={styles.menuIcon} />
+            <span>Logout</span>
+          </button>
+        )}
+      </div>
     </div>
-  </div>
-);
+  )
+};
