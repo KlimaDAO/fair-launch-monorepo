5f895817359b2f12c39b1209208643f4ca4e5173
0xMakka <keith.33m@gmail.com>
2025-03-24 13:15:55 +0000

    fix: mobile navbar menu

--- fair-launch/components/navbar/index.tsx
+++ fair-launch/components/navbar/index.tsx
@@ -1,37 +1,57 @@
 'use client';
 
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import Image from "next/image";
 import klimaLogo from "@public/kp-logo.svg";
 import { LuMenu } from "react-icons/lu";
 import * as styles from "./styles";
-import { MdLogout } from "react-icons/md";
+import { MdClose, MdLogout } from "react-icons/md";
 import { useAccount, useDisconnect } from "wagmi";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export const Navbar: FC = () => {
   const router = useRouter();
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = async () => {
     await disconnect();
+    setIsMenuOpen(!isMenuOpen);
     setTimeout(() => router.push('/'), 100);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <div className={styles.navbar}>
       <Image src={klimaLogo} alt="Klima Protocol Logo" />
       <div className={styles.menuContainer}>
-        {/* <LuMenu className={styles.menuIcon} /> */}
         {address && (
-          <button
-            onClick={handleLogout}>
+          <button onClick={toggleMenu}>
             <LuMenu className={styles.menuIcon} />
-            <span>Logout</span>
           </button>
         )}
       </div>
+
+      {isMenuOpen && (
+        <div className={styles.fullScreenMenu}>
+          <button onClick={toggleMenu} className={styles.closeButton}>
+            <MdClose fontSize="2.8rem" />
+          </button>
+          <nav className={styles.nav}>
+            <ul>
+              <li><Link href="/">Home</Link></li>
+              <li><Link href="/my-rewards">My Rewards</Link></li>
+              <li><Link href="/protocol">Protocol</Link></li>
+              <li><div onClick={handleLogout}>Logout</div></li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </div>
   )
 };
--- fair-launch/components/navbar/styles.ts
+++ fair-launch/components/navbar/styles.ts
@@ -28,3 +28,50 @@ export const menuIcon = css({
   fontSize: '2rem',
   color: 'white',
 });
+
+export const fullScreenMenu = css({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(255,255,255,0.98)',
+  color: 'black',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  padding: '2rem',
+  justifyContent: 'flex-start',
+  zIndex: 1000,
+});
+
+export const closeButton = css({
+  position: 'absolute',
+  top: '1.8rem',
+  right: '2.2rem',
+  background: 'none',
+  border: 'none',
+  color: 'black',
+  fontSize: '2rem',
+  cursor: 'pointer',
+});
+
+export const nav = css({
+  listStyle: 'none',
+  padding: '0',
+  marginTop: '6.4rem',
+
+  '& li': {
+    margin: '1.2rem 0',
+
+    '& a': {
+      color: 'black',
+      textDecoration: 'none',
+      fontSize: '2.4rem',
+    }
+  },
+
+  '& div': {
+    fontSize: '2.4rem',
+  }
+});
