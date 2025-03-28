df9e635c952d3ccfa9f90b27a28f5843d1238e55
0xMakka <keith.33m@gmail.com>
2025-03-28 11:16:31 +0000

    fix: response headers in middleware

--- fair-launch/middleware.ts
+++ fair-launch/middleware.ts
@@ -1,6 +1,11 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   const cookie = request.cookies.get('wagmi.store');
   const { pathname } = request.nextUrl;
 
