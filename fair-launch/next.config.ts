8d2f4d00c2e0dafea4b1de564d66af068990248b
0xMakka <keith.33m@gmail.com>
2025-03-28 11:20:26 +0000

    fix: add headers

--- fair-launch/next.config.ts
+++ fair-launch/next.config.ts
@@ -5,6 +5,39 @@ const nextConfig: NextConfig = {
   experimental: {
     useCache: true
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
