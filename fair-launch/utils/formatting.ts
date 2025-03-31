d3906c2be7d82d5de7f9c2495021e98898f49635
0xMakka <keith.33m@gmail.com>
2025-03-31 17:19:17 +0100

    fix: wallet address truncation

--- fair-launch/utils/formatting.ts
+++ fair-launch/utils/formatting.ts
@@ -53,7 +53,7 @@ export const formatValueToNumber = (value: number | string | bigint) =>
  */
 export const truncateAddress = (address: string): string => {
   if (address.length <= 10) return address;
-  return `${address.slice(0, 5)}...${address.slice(-3)}`;
+  return `${address.slice(0, 6)}...${address.slice(-4)}`;
 };
 
 /**
