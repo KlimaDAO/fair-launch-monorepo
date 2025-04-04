e45a652dcc365fd790e082634da9573daa06d25e
0xMakka <keith.33m@gmail.com>
2025-04-04 15:23:04 +0100

    revert back to readContract and add delays

--- fair-launch/app/api/leaderboards/route.ts
+++ fair-launch/app/api/leaderboards/route.ts
@@ -2,57 +2,55 @@ import { abi as klimaFairLaunchAbi } from "@abi/klima-fair-launch";
 import { getConfig } from "@utils/constants";
 import { fetchLeaderboard } from "@utils/queries";
 import { config as wagmiConfig } from "@utils/wagmi.server";
-import { readContracts } from "@wagmi/core";
+import { readContract } from "@wagmi/core";
 import { omit } from "lodash";
 import { NextResponse } from "next/server";
-import { AbiFunction, formatUnits } from "viem";
+import { formatUnits } from "viem";
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 const calculateLeaderboard = async () => {
-  const results: any[] = [];
+  const results = [];
   const config = getConfig();
   const leaderboard = await fetchLeaderboard(1000);
-
-  const allPoints = await readContracts(wagmiConfig, {
-    contracts: leaderboard?.wallets?.map((wallet) => ({
-      args: [wallet.id],
-      abi: klimaFairLaunchAbi as AbiFunction[],
-      address: config.fairLaunchContractAddress,
-      functionName: "previewUserPoints",
-    })) || [],
-  });
-  const allPointsArray = allPoints.flatMap(({ result }) => ({ points: result }));
-
-  await Promise.all((leaderboard?.wallets || []).map(async (wallet, index) => {
+  for (const wallet of leaderboard.wallets || []) {
     try {
-      const userStakesLength = new Array(Number(wallet.stakes.length)).fill("");
-      const userStakesData = await readContracts(wagmiConfig, {
-        contracts: userStakesLength.map((_, index) => ({
-          abi: klimaFairLaunchAbi as AbiFunction[],
-          address: config.fairLaunchContractAddress,
-          functionName: "userStakes",
-          args: [wallet.id, index],
-        })),
-      });
-      const userStakes = userStakesData
-        .flatMap(({ result }: any) => result?.[0] ?? 0);
+      const userStakesInfo = await Promise.all(
+        (wallet?.stakes || []).map(async (_, index) => {
+          await delay(8); // Delay to respect the 125 requests per second limit
+          const [amount] = (await readContract(wagmiConfig, {
+            abi: klimaFairLaunchAbi,
+            address: config.fairLaunchContractAddress,
+            functionName: "userStakes",
+            args: [wallet.id, index],
+          })) as bigint[];
+          return amount;
+        })
+      );
 
-      const totalStaked = userStakes.reduce(
+      const totalStaked = userStakesInfo.reduce(
         (acc, curr) => acc + Number(curr),
         0
       );
 
+      await delay(8); // Delay to respect the 125 requests per second limit
+      const points = await readContract(wagmiConfig, {
+        args: [wallet.id],
+        abi: klimaFairLaunchAbi,
+        address: config.fairLaunchContractAddress,
+        functionName: "previewUserPoints",
+      });
+
       results.push({
         ...omit(wallet, "stakes"),
         totalStaked,
-        totalPoints: Number(formatUnits(allPointsArray[index].points as any, 18)),
+        totalPoints: Number(formatUnits(points as bigint, 18)),
       });
     } catch (error) {
       console.error(`Error fetching points for ${wallet.id}:`, error);
-        results.push({ ...omit(wallet, "stakes"), totalPoints: null });
-      }
-    })
-  );
-
+      results.push({ ...omit(wallet, "stakes"), totalPoints: null });
+    }
+  }
   return results
     .sort((a, b) => Number(b.totalPoints) - Number(a.totalPoints))
     .slice(0, 150);
@@ -63,7 +61,7 @@ export async function GET() {
   const response = NextResponse.json(leaderboardData);
   response.headers.set(
     "Cache-Control",
-    "public, max-age=100, stale-while-revalidate=1800"
+    "public, max-age=10, stale-while-revalidate=1800"
   );
   return response;
 }
