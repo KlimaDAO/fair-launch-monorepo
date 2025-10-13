1612737652de0a1b80a4b9f39e2b141588271e9b
must-be-carbon <175485679+must-be-carbon@users.noreply.github.com>
2025-10-13 18:01:46 +0530

    feat: update deployment script

--- solidity/script/deployments/001-deploy-stakingV3.s.sol
+++ solidity/script/deployments/001-deploy-stakingV3.s.sol
@@ -4,11 +4,10 @@ pragma solidity ^0.8.28;
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 import {KlimaFairLaunchStaking} from "../../src/KlimaFairLaunchStaking.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 // This script will deploy implementation for staking v3
 // The upgrade will be done in a separate
-contract UpgradeStakingToV3 is Script {
+contract DeployStakingV3Implementation is Script {
     function run() public {
         vm.startBroadcast();
         console2.log("Deploying KlimaFairLaunchStaking implementation...");
