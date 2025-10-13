7ab4af3290656fce5df0a70cc79a9cee206334df
must-be-carbon <175485679+must-be-carbon@users.noreply.github.com>
2025-10-13 18:01:37 +0530

    feat: update deployment script

--- solidity/script/deployments/001-deploy-stakingV3.s.sol
+++ solidity/script/deployments/001-deploy-stakingV3.s.sol
@@ -6,39 +6,17 @@ import {console2} from "forge-std/console2.sol";
 import {KlimaFairLaunchStaking} from "../../src/KlimaFairLaunchStaking.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-
 // This script will deploy implementation for staking v3
-// The upgrade will be done in a separate 
+// The upgrade will be done in a separate
 contract UpgradeStakingToV3 is Script {
     function run() public {
         vm.startBroadcast();
-
-        address staking = vm.envAddress("STAKING_CONTRACT_ADDRESS");
-        address user = address(0xBA0111F5EC1B6f2F092e9730F5F64840f3B42C95);
-
-        KlimaFairLaunchStaking stakingOld = KlimaFairLaunchStaking(staking);
-        address stakingOwner = stakingOld.owner();
-
-        uint256 userStakesLength = stakingOld.getUserStakeCount(user);
-
-        console2.log("Staking contract address:", staking);
-        console2.log("Staking owner:", stakingOwner);
-
-        console2.log("UpgradeStaking deployed");
+        console2.log("Deploying KlimaFairLaunchStaking implementation...");
         KlimaFairLaunchStaking newImplementation = new KlimaFairLaunchStaking();
-        console2.log("Staking deployed at:", address(staking));
-
-        // vm.startPrank(stakingOwner);
-        // stakingOld.upgradeToAndCall(address(newImplementation), "");
-        // vm.stopPrank();
-
-        // KlimaFairLaunchStaking.Stake[] memory userStakes = stakingOld
-        //     .getUserStakes(user);
-
-        // uint256 userStakesLengthAfter = stakingOld.getUserStakes(user).length;
-
-        // console2.log("User stakes length after:", userStakesLengthAfter);
-        // console2.log("User stakes length before:", userStakesLength);
+        console2.log(
+            "KlimaFairLaunchStaking implementation deployed at:",
+            address(newImplementation)
+        );
 
         vm.stopBroadcast();
     }
